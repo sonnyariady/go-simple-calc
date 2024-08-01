c8b64ca4114b2ad836a54259f5cc1dafc2f0343f
Sonny Ariady <sonnyariady@gmail.com>
2024-08-01 23:22:15 +0700

    update modul

--- simplecalc.go
+++ simplecalc.go
@@ -1,17 +1,23 @@
 package mysimplecalc
 
-func Jumlah(a, b float32) float32 {
+import "math"
+
+func Jumlah(a, b float64) float64 {
 	return a + b
 }
 
-func Kali(a, b float32) float32 {
+func Kali(a, b float64) float64 {
 	return a * b
 }
 
-func Kurang(a, b float32) float32 {
+func Kurang(a, b float64) float64 {
 	return a - b
 }
 
-func Bagi(a, b float32) float32 {
+func Bagi(a, b float64) float64 {
 	return a / b
+}
+
+func Pitagoras(a, b float64) float64 {
+	math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
 }
