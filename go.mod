08c7dfc9a8d488635ca950b4dc4de21b58ad2f2e
Sonny Ariady <sonnyariady@gmail.com>
2024-08-01 22:55:00 +0700

    buat modul simple calc

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sonnyariady/go-simple-calc
+
+go 1.22.5
---
+++ simplecalc.go
@@ -0,0 +1,17 @@
+package mysimplecalc
+
+func Jumlah(a, b float32) float32 {
+	return a + b
+}
+
+func Kali(a, b float32) float32 {
+	return a * b
+}
+
+func Kurang(a, b float32) float32 {
+	return a - b
+}
+
+func Bagi(a, b float32) float32 {
+	return a / b
+}
