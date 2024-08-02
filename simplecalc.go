7e44ffb9386c5a4c1f18ba17d92195e916f41db6
Sonny Ariady <sonnyariady@gmail.com>
2024-08-02 10:07:00 +0700

    update fungsi baru

--- simplecalc.go
+++ simplecalc.go
@@ -21,3 +21,7 @@ func Bagi(a, b float64) float64 {
 func Pitagoras(a, b float64) float64 {
 	return math.Sqrt(math.Pow(a, 2) + math.Pow(b, 2))
 }
+
+func JumlahKuadrat(a, b float64) float64 {
+	return math.Pow(a, 2) + math.Pow(b, 2)
+}
