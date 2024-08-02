f4724d3f3b3c78925924dcf36fdd9f2526dd46b7
Sonny Ariady <sonnyariady@gmail.com>
2024-08-02 21:20:18 +0700

    comit utk cara unit test yg gagal

--- simplecalc_test.go
+++ simplecalc_test.go
@@ -1,31 +1,43 @@
 package mysimplecalc
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestJumlah(t *testing.T) {
 	hasil := Jumlah(4, 2)
 	if hasil != 6 {
-		panic("Hasilnya salah")
+		t.Error("Hasilnya salah")
 	}
 }
 
 func TestKali(t *testing.T) {
 	hasil := Kali(13, 3)
 	if hasil != 39 {
-		panic("Hasilnya salah")
+		t.Error("Hasilnya salah")
 	}
 }
 
-func TestPitagoras(t *testing.T) {
-	hasil := Pitagoras(3, 4)
+func TestPitagoras1(t *testing.T) {
+	hasil := Pitagoras(3, 5)
 	if hasil != 5 {
-		panic("Hasilnya salah")
+		t.Error("Hasilnya salah, yg benar ", 5, "tapi hasilnya", hasil)
 	}
+	fmt.Println("Dieksekusi")
+}
+
+func TestPitagoras2(t *testing.T) {
+	hasil := Pitagoras(3, 5)
+	if hasil != 5 {
+		t.Fatal("Hasilnya salah, yg benar ", 5, "tapi hasilnya", hasil)
+	}
+	fmt.Println("Tidak Dieksekusi")
 }
 
 func TestJumlahKuadrat(t *testing.T) {
 	hasil := JumlahKuadrat(3, 4, 5)
 	if hasil != 50 {
-		panic("Hasilnya salah")
+		t.Error("Hasilnya salah")
 	}
 }
